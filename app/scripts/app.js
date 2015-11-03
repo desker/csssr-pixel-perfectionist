ef149a9cad948828ec58ea2c20d87489ec5a672f
Sergey Karinskiy <sergey@karinskiy.ru>
2015-11-03 07:18:20 +0600

    Desktop

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,31 @@ import 'svg4everybody';
 import $ from 'jquery';
 
 $(() => {
-	// Code here
+	const baseLeft = $('.slider__bar').offset().left;
+	const baseWidth = $('.slider__bar').width();
+	const slide = $('.slider__value');
+	let isSlide = false;
+
+	slide.mousedown(() => {
+		isSlide = true;
+	});
+
+	$(window).mousemove(e => {
+		if (!isSlide) {
+			return;
+		}
+		let offset = e.pageX - baseLeft;
+		if (offset > baseWidth) {
+			offset = baseWidth - 2;
+		}
+		if (offset < 0) {
+			offset = 0;
+		}
+		slide.css({left: offset});
+	});
+
+	$(window).mouseup(() => {
+		isSlide = false;
+	});
+
 });
